dfae7ff714dc25f237a9670abcad341a305d4e79
ipodtouchdude <ipodtouchdude@me.com>
2020-12-29 23:16:07 +0000

    Initial Commit

---
+++ iOS 2 M1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS 2 M1
+//
+//  Created by ipodtouchdude on 29/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS 2 M1/iOS_2_M1App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_2_M1App.swift
+//  iOS 2 M1
+//
+//  Created by ipodtouchdude on 29/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct iOS_2_M1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
